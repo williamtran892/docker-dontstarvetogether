d39a2f916d35516b6606f4822bad39a8ad2b88e1
TheOwl <williamtran892@outlook.com>
2017-09-19 21:26:45 +0800

    Add my-world-settings

--- my-world-settings/modoverrides.lua
+++ my-world-settings/modoverrides.lua
@@ -3,7 +3,7 @@ return {
 	['workshop-492173795'] = { enabled = true },
 	['workshop-407705132'] = { enabled = true,
 		configuration_options = {
-			title = 'Welcome!',
+			title = 'Ham choi vl!',
 			message = 'Sharing is caring! Play fair!',
 		},
 	},
